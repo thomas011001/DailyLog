347e68b03d9ab303a3c983b05c5e212d56c7a4d8
thomas011001 <thomasyacoub321@gmail.com>
2026-03-30 13:57:14 +0200

    Add auth routes

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from core import views
+
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("signup", views.signup, name="signup" ),
+    path("login", views.login, name="login"),
+    path("logout", views.logout, name="logout")
+]
