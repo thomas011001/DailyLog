c827a95f5d868ce25d60e1d426d0b7ae6a125e2f
thomas011001 <thomasyacoub321@gmail.com>
2026-03-30 13:56:50 +0200

    Add auth views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,88 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 from django.shortcuts import render
+from django import forms
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
-# Create your views here.
+
+@login_required
+def index(request):
+  return render(request, "core/index.html")
+
+class SignUpForm(forms.Form):
+  first_name = forms.CharField(label="First Name", max_length=50, min_length=2)
+  last_name = forms.CharField(label="Last Name", max_length=50, min_length=2)
+  username = forms.CharField(label="Username",max_length=50, min_length=3)
+  password = forms.CharField(label="Password", widget=forms.PasswordInput(), min_length=8)
+
+  def clean_username(self):
+    username = self.cleaned_data["username"]
+    if " " in username: 
+      raise forms.ValidationError("Username can't have spaces")
+    
+    if User.objects.filter(username=username).exists():
+      raise forms.ValidationError("This username is already taken, please choose another.")
+
+    return username
+
+def signup(request):
+  if request.user.is_authenticated:
+        return redirect('index')
+
+  if request.method == "POST":
+        form = SignUpForm(request.POST)
+        
+        if form.is_valid():
+            data = form.cleaned_data
+            
+            new_user = User.objects.create_user(
+                username=data["username"],
+                first_name=data["first_name"],
+                last_name=data["last_name"],
+                password=data["password"]
+            )
+            return redirect("login")
+        return render(request, 'core/signup.html', {
+          "form": form,
+        })
+
+  form = SignUpForm()     
+  return render(request, 'core/signup.html', {
+    "form": form,
+  })
+
+class LoginForm(forms.Form):
+  username = forms.CharField(label="Username", required=True)
+  password = forms.CharField(label="Password",widget=forms.PasswordInput() , required=True)
+
+def login(request):
+  if request.user.is_authenticated:
+        return redirect('index')
+
+  if request.method == "POST":
+    form = LoginForm(request.POST)
+    
+    body = request.POST
+    username = body.get("username")
+    password = body.get("password")
+
+    user = authenticate(request, password=password, username=username)
+    if user:
+      auth_login(request, user)
+      return redirect("index")
+
+    return render(request, "core/login.html", {
+      "form": form,
+      "errors": "Invalid username or password."
+    })
+  
+  form = LoginForm()
+  return render(request, "core/login.html", {
+    "form": form
+  })
+
+@login_required
+def logout(request):
+  auth_logout(request)
+  return redirect("login")
