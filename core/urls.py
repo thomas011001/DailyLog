0c7cd69786db301ce4403ae83599eee8dd5eff30
thomas011001 <thomasyacoub321@gmail.com>
2026-04-03 10:52:39 +0200

    add url pattern for getting day details

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("signup", views.signup, name="signup" ),
     path("login", views.login, name="login"),
     path("logout", views.logout, name="logout"),
-    path("day", views.day, name="day")
+    path("day", views.day, name="day"),
+    path("day/<int:id>", views.day, name="day-detail"),
 ]
