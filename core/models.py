c777c81062fece1b24fc2cce41d9b7309fd96f48
thomas011001 <thomasyacoub321@gmail.com>
2026-04-06 00:17:19 +0200

    Add Task model with foreign key to Day and completion status

--- core/models.py
+++ core/models.py
@@ -8,3 +8,11 @@ class Day(models.Model):
 
   class Meta:
     unique_together = ("owner", "date")
+
+class Task(models.Model):
+  day = models.ForeignKey(Day, on_delete=models.CASCADE)
+  title = models.CharField(max_length=50)
+  is_complete = models.BooleanField(default=False)
+
+  def __str__(self) -> str:
+    return f"{self.title}: {self.is_complete}"
